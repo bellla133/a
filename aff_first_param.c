9c025a3e795fed5a4c40b58032400b4f9b107048
ayoub <ayoub@1337>
2025-07-25 23:28:50 +0100

    aff_first_param.c

--- aff_first_param.c
+++ aff_first_param.c
@@ -1,24 +1,24 @@
 #include <unistd.h>
 
 
-int main (int argv , char **agrc )
+int main (int argv , char **argc )
 {
 	if(argv >=2)
 	{
 		int	i=0;
-		while(argc[1][i]= '\0')
+		while(argc[1][i] != '\0')
 		{
-			char j[] =argc[1][i];
-			write(1, &j[], 1);
+			write(1, &argc[1][i], 1);
 			i++;
 		}
+		write(1, "\n", 1);
 	}
 	else if(argv == 1)	
 	{
-		write(1, "\n");
+		write(1, "\n", 1);
 	}
 
-
+return 0;
 }
 
 
